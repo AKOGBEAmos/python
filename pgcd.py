6ccc0ae59be54074cab9cde099d5c89356d5db5d
AKOGBEAmos <104907828+AKOGBEAmos@users.noreply.github.com>
2023-06-30 10:38:13 +0100

    Update pgcd.py

--- pgcd.py
+++ pgcd.py
@@ -1,16 +1,20 @@
-def pgcd(a,b):
-    if a<b:
-        c=a
-        a=b
-        b=c
-    elif a==0:
-        return b
-    r=a%b
+def pgcd(number1,number2):
+    #On vérifie que la division est possible en faisant une comparaison i.e il faut que number1>number2)
+    if number1<number2:
+        var=number1
+        number1=number2
+        number2=var
+    elif number1==0:
+        """Si number1 est nul alors directement le pgcd=number2 
+            car, O est multiple de tous les nombres."""
+        return number2
+        #On utilise l'algorithme d'Euclide
+    r=number1%number2
     while(r!=0):
-        a=b
-        b=r
-        r=a%b
-    return b
+        number1=number2
+        number2=r
+        r=number1%number2
+    return number2
     
 //Juste un code pour tester la fonction.
 
